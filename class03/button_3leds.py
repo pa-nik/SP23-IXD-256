f92ad761c9c465599d70a3384b1b16a74a471a0f
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-03 11:42:39 -0800

    add timer and state variables

--- class03/button_3leds.py
+++ class03/button_3leds.py
@@ -1,23 +1,29 @@
 from machine import Pin
-from time import sleep_ms
+from time import *
 
 led_pin_green = Pin(32, Pin.OUT)           # create output pin on G32 (4-pin bottom connector white wire)
 led_pin_yellow = Pin(21, Pin.OUT)          # create output on pin G21 (4-pin back connector)
 led_pin_red = Pin(25, Pin.OUT)             # create output on pin G25 (4-pin back connector)
 button_pin = Pin(26, Pin.IN, Pin.PULL_UP)  # create pull-up input on pin G26 (yellow wire)
 
+timer_ms = ticks_ms()  # create a timer variable and save current time
+program_state = 'GREEN'
+
 while True:
-    led_pin_green.on()
-    sleep_ms(500)
-    led_pin_green.off()
-    sleep_ms(500)
-
-    led_pin_yellow.on()
-    sleep_ms(500)
-    led_pin_yellow.off()
-    sleep_ms(500)
-
-    led_pin_red.on()
-    sleep_ms(500)
-    led_pin_red.off()
-    sleep_ms(500)
+    if(ticks_ms() > timer_ms + 500):
+        if(program_state == 'GREEN'):
+            led_pin_green.off()
+            led_pin_yellow.on()
+            program_state = 'YELLOW'
+            print(program_state)
+        elif(program_state == 'YELLOW'):
+            led_pin_yellow.off()
+            led_pin_red.on()
+            program_state = 'RED'
+            print(program_state)
+        elif(program_state == 'RED'):
+            led_pin_red.off()
+            led_pin_green.on()
+            program_state = 'GREEN'
+            print(program_state)
+        timer_ms = ticks_ms()
