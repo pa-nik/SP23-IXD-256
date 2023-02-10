385daf5ae50784c03deb98fc3e77ef143b2a306d
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-10 10:49:49 -0800

    add map_value function

--- class04/adc_read.py
+++ class04/adc_read.py
@@ -5,7 +5,18 @@ analog_pin = Pin(32, Pin.IN)  # configure input on pin G32 (white wire)
 adc = ADC(analog_pin)  # create analog-to-digital converter (ADC) input
 adc.atten(ADC.ATTN_11DB)  # set 11dB attenuation (2.45V range)
 
+# map an input value (v_in) between min/max ranges:
+def map_value(in_val, in_min, in_max, out_min, out_max):
+    v = out_min + (in_val - in_min) * (out_max - out_min) / (in_max - in_min)
+    if (v < out_min): 
+        v = out_min 
+    elif (v > out_max): 
+        v = out_max
+    return int(v)
+
 while True:
     analog_val = adc.read()  # read 12-bit analog value (0 - 4095 range)
-    print(analog_val)
+    analog_val_8bit = map_value(analog_val, in_min = 0, in_max = 4095, out_min = 0, out_max = 255)
+    #print(analog_val)
+    print(analog_val_8bit)
     sleep_ms(100)
