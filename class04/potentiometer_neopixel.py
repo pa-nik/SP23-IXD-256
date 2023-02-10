6cc540a8241132d0d101563f8c27b03549c965bc
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-10 11:27:15 -0800

    update potentiometer neopixel example

--- class04/potentiometer_neopixel.py
+++ class04/potentiometer_neopixel.py
@@ -22,9 +22,22 @@ while True:
     analog_val = adc.read()  # read 12-bit analog value (0 - 4095 range)
     analog_val_8bit = map_value(analog_val, in_min = 0, in_max = 4095, out_min = 0, out_max = 255)
     #print(analog_val)
-    print(analog_val_8bit)
+    #print(analog_val_8bit)
+    # changing how many neopixels are colored using ADC value:
+    # map ADC value from 0 - 4095 range to 0 - 30
+    analog_val_30 = map_value(analog_val, 0, 4095, out_min = 0, out_max = 30)
+    print(analog_val_30)
+    for pixel_index in range(30):
+        if(pixel_index < analog_val_30):  # pixel index is less than ADC value
+            neopixel_strip[pixel_index] = (255, 0, 0)
+        else:
+            neopixel_strip[pixel_index] = (0, 0, 0)
+    neopixel_strip.write()  # write color data to neopixels
+    '''
+    # change of brightness of 30 pixels using ADC value:
     for pixel_index in range(30):
         r = (analog_val_8bit, 0, 0)  # red color that changes with ADC value
         neopixel_strip[pixel_index] = r  # assign pixel to red color
     neopixel_strip.write()  # write color data to neopixels
+    '''
     sleep_ms(100)
