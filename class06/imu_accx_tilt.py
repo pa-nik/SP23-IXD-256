474929d01113d4a222e9af91db720bcf8779f1a1
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-24 12:20:53 -0800

    add tilt conditions and program state

--- class06/imu_accx_tilt.py
+++ class06/imu_accx_tilt.py
@@ -9,7 +9,19 @@ imu_sensor = imu.IMU()
 left_arrow = [0,0,0,0xffffff,0,0,0,0xffffff,0,0,0,0xffffff,0,0,0,0,0,0xffffff,0,0,0,0,0,0xffffff,0]
 right_arrow = [0,0xffffff,0,0,0,0,0,0xffffff,0,0,0,0,0,0xffffff,0,0,0,0xffffff,0,0,0,0xffffff,0,0,0]
 
+program_state = 'RIGHT'
+
 while True:
     acc_x = imu_sensor.acceleration[0]
-    print(acc_x)
+    #print(acc_x)
+    if(program_state == 'RIGHT'):
+        if(acc_x < -0.25):
+            program_state = 'LEFT'
+            rgb.set_screen(left_arrow)
+            print('tilt left')
+    elif(program_state == 'LEFT'):
+        if(acc_x > 0.25):
+            program_state = 'RIGHT'
+            rgb.set_screen(right_arrow)
+            print('tilt right')
     sleep_ms(100)
