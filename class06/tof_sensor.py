51cd56c3216c83c5a64fe6dec310e0f236888854
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-24 11:04:49 -0800

    display question mark

--- class06/tof_sensor.py
+++ class06/tof_sensor.py
@@ -5,6 +5,13 @@ from time import *
 from neopixel import NeoPixel
 
 tof_sensor = unit.get(unit.TOF, unit.PORTA)
+question_mark = [
+    0, 0xff0000, 0xff0000, 0xff0000, 0,
+    0, 0,        0,        0xff0000, 0,
+    0, 0,        0xff0000, 0xff0000, 0,
+    0, 0,        0,        0,        0,
+    0, 0,        0xff0000, 0,        0]
+rgb.set_screen(question_mark)
 
 neopixel_pin = Pin(27, Pin.OUT)  # configure output on pin G27 (atom matrix display)
 neopixel_display = NeoPixel(neopixel_pin, 25)  # create NeoPixel object with 25 pixels
@@ -23,11 +30,11 @@ while True:
     brightness = map_value(d, in_min = 0, in_max = 8192, out_min = 0, out_max = 255)
     #print(d)
     print(brightness)
-    
-    # change of brightness of 25 pixels using ADC value:
+    '''
+    # change of brightness of 25 pixels using ToF value:
     for pixel_index in range(25):
         r = (brightness, 0, 0)  # red color that changes with ADC value
         neopixel_display[pixel_index] = r  # assign pixel to red color
     neopixel_display.write()  # write color data to neopixels
-
+    '''
     sleep_ms(50)
