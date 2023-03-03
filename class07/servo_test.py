e44ae10a1c95f7ca4ac4b5d1a138897938b23b5c
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-03-03 11:15:58 -0800

    add analog input to servo example

--- class07/servo_test.py
+++ class07/servo_test.py
@@ -1,8 +1,18 @@
 from m5stack import *
 from time import *
-from machine import Pin
+from machine import Pin, ADC
 from servo import Servo
 
+analog_pin = Pin(32, Pin.IN)  # configure input on pin G32 (white wire)
+adc = ADC(analog_pin)  # create analog input on analog_pin
+adc.atten(ADC.ATTN_11DB)  # enable full-range on ADC
+
+while True:
+    analog_val = adc.read()  # read analog value (0 - 4095 range)
+    print(analog_val)
+    sleep_ms(100)
+
+print('servo test..')
 servo_obj = Servo(Pin(26))  # create a Servo object on G26 (yellow wire)
 servo_obj.write_us(1400)  # move clockwise
 sleep_ms(2000)
