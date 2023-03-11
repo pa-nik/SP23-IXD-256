2785b85f97ac169fcd8cfede1ffa420d44ab1f10
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-03-11 13:27:02 -0800

    update mqtt publish example

--- class08/mqtt_publish_test.py
+++ class08/mqtt_publish_test.py
@@ -14,19 +14,19 @@ wifiCfg.doConnect('INSERT_NETWORK_NAME', 'INSERT_NETWORK_PASSWORD')
 
 # create the MQTT feed by inserting your Adafruit IO username and key:
 mqtt_feed = M5mqtt(
-    'testclient',  # any name for your device (MQTT client)
-    'io.adafruit.com',  # MQTT broker address
-    1883,  # port to use for the connection
-    'INSERT_ADAFRUIT_IO_USERNAME',  # your Adafruit IO username 
-    'INSERT_ADAFRUIT_IO_KEY',  # your Adafruit IO key (check yellow key icon)
-    300
+    client_id = 'testclient',  # any name for your device (MQTT client)
+    server = 'io.adafruit.com',  # MQTT broker address
+    port = 1883,  # port to use for the connection
+    user = 'INSERT_ADAFRUIT_IO_USERNAME',  # your Adafruit IO username 
+    password = 'INSERT_ADAFRUIT_IO_KEY',  # your Adafruit IO key (check yellow key icon)
+    keepalive = 300  # keep alive timeout  
 )
 mqtt_feed.start()
 
 while True:
     analog_val = adc.read()  # read analog value (0 - 4095 range)
     print(analog_val)
-    # change to your feed name publish sensor data to the feed:
+    # change your username and feed name to publish sensor data to the feed:
     mqtt_feed.publish('INSERT_ADAFRUIT_IO_USERNAME/feeds/INSERT_ADAFRUIT_IO_FEED', str(analog_val))
     sleep_ms(3000)
 
