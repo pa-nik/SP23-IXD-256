73100053d8adc96948f5e6cbf88f0dfe8a16e224
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-04-07 08:16:59 -0700

    ble keyboard number keys codes

--- extras/ble_keyboard_example.py
+++ extras/ble_keyboard_example.py
@@ -103,9 +103,83 @@ adv = (
 conn_handle = None
 ble.gap_advertise(100_000, adv)
 
+'''
+# key codes:
+0x04	Keyboard a and A
+...
+0x1D	Keyboard z and Z
+0x1E	Keyboard 1 and !
+0x1F	Keyboard 2 and @
+0x20	Keyboard 3 and #
+0x21	Keyboard 4 and $
+0x22	Keyboard 5 and %
+0x23	Keyboard 6 and ^
+0x24	Keyboard 7 and &
+0x25	Keyboard 8 and *
+0x26	Keyboard 9 and (
+0x27	Keyboard 0 and )
+0x28	Keyboard Return (ENTER)
+0x29	Keyboard ESCAPE
+0x2A	Keyboard DELETE (Backspace)
+0x2B	Keyboard Tab
+0x2C	Keyboard Spacebar
+0x2D	Keyboard - and (underscore)
+0x2E	Keyboard = and +
+0x2F	Keyboard [ and {
+0x30	Keyboard ] and }
+0x31	Keyboard \ and |
+0x32	Keyboard Non-US # and ~
+0x33	Keyboard ; and :
+0x34	Keyboard ' and "
+0x35	Keyboard Grave Accent and Tilde
+0x36	Keyboard, and <
+0x37	Keyboard . and >
+0x38	Keyboard / and ?
+0x39	Keyboard Caps Lock
+0x3A	Keyboard F1
+0x3B	Keyboard F2
+0x3C	Keyboard F3
+0x3D	Keyboard F4
+0x3E	Keyboard F5
+0x3F	Keyboard F6
+0x40	Keyboard F7
+0x41	Keyboard F8
+0x42	Keyboard F9
+0x43	Keyboard F10
+0x44	Keyboard F11
+0x45	Keyboard F12
+0x46	Keyboard PrintScreen
+0x47	Keyboard Scroll Lock
+0x48	Keyboard Pause
+0x49	Keyboard Insert
+0x4A	Keyboard Home
+0x4B	Keyboard PageUp
+0x4C	Keyboard Delete Forward
+0x4D	Keyboard End
+0x4E	Keyboard PageDown
+0x4F	Keyboard RightArrow
+0x50	Keyboard LeftArrow
+0x51	Keyboard DownArrow
+0x52	Keyboard UpArrow
+0x53	Keypad Num Lock and Clear
+0x54	Keypad /
+0x55	Keypad *
+0x56	Keypad -
+0x57	Keypad +
+0x58	Keypad ENTER
+0x59	Keypad 1 and End
+0x5A	Keypad 2 and Down Arrow
+0x5B	Keypad 3 and PageDn
+0x5C	Keypad 4 and Left Arrow
+0x5D	Keypad 5
+0x5E	Keypad 6 and Right Arrow
+0x5F	Keypad 7 and Home
+0x60	Keypad 8 and Up Arrow
+0x61	Keypad 9 and PageUp
+0x62	Keypad 0 and Insert
+0x63	Keypad . and Delete
+'''
 # once connected use the following to send reports
-
-
 def send_char(char):
     if char == " ":
         mod = 0
@@ -116,6 +190,12 @@ def send_char(char):
     elif ord("A") <= ord(char) <= ord("Z"):
         mod = 2
         code = 0x04 + ord(char) - ord("A")
+    elif char == "0":
+        mod = 0
+        code = 0x27
+    elif ord("1") <= ord(char) <= ord("9"):
+        mod = 0
+        code = 0x1E + ord(char) - ord("1")
     else:
         assert 0
     ble.gatts_notify(conn_handle, h_rep, struct.pack("8B", mod, 0, code, 0, 0, 0, 0, 0))
@@ -131,7 +211,7 @@ print('start loop..')
 while True:
     if (conn_handle != None):
         print('send char..')
-        send_char('b')
+        send_char('0')
         sleep_ms(500)
     else:
         print('waiting..')
