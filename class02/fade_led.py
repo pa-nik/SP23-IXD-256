b767cc8c5704fee157c3eb8779bde204d91d4b17
Nikita Pashenkov <nik@aeolab.com>
2023-08-09 19:52:59 -0700

    change wait_ms to sleep_ms

--- class02/fade_led.py
+++ class02/fade_led.py
@@ -2,13 +2,18 @@
 
 from m5stack import *
 from machine import Pin, PWM
+from time import *
 
 led_pwm = PWM(Pin(32))          # create PWM output on pin G32 (white wire)
 
 while True:
   for i in range(100):
     led_pwm.duty(i)             # set PWM duty cycle 
-    wait_ms(10)                 # delay 
+    #wait_ms(10)                 # delay 
+    sleep_ms(10)
   for i in range(100):
     led_pwm.duty(100-i)         # set PWM duty cycle 
-    wait_ms(10)                 # delay 
+    #wait_ms(10)                 # delay 
+    sleep_ms(10)
+  sleep_ms(100)
+  print('hello!')
